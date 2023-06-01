dd7676d56584dba6407fc479c4e6ddcef0ca0b9e
jta666 <kuba.adamiak@interia.eu>
2023-06-01 21:43:09 +0200

    account system added

--- pacman/core/urls.py
+++ pacman/core/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 
 from . import views
 from .views import *
@@ -11,4 +12,9 @@ urlpatterns = [
     path("game/", views.game, name="game"),
     path("login/", MyLoginView.as_view(), name="login"),
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('user_maps/', views.user_maps, name='user_maps'),
+    path('public_maps/', views.public_maps, name='public_maps'),
+    path('toggle_public/<int:map_id>/', views.toggle_public, name='toggle_public'),
+    path('like_map/<int:map_id>/', views.like_map, name='like_map'),
+    path('tasks/', views.tasks, name='tasks'),
 ]
--- pacman/core/views.py
+++ pacman/core/views.py
@@ -19,7 +19,7 @@ class MyLoginView(LoginView):
     redirect_authenticated_user = True
     
     def get_success_url(self):
-        return reverse_lazy('tasks') 
+        return reverse_lazy('core:tasks') 
     
     def form_invalid(self, form):
         messages.error(self.request,'Invalid username or password')
@@ -27,5 +27,44 @@ class MyLoginView(LoginView):
     
 class SignUpView(CreateView):
     form_class = SignUpForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('core:login')
     template_name = 'registration/signup.html'
+
+from django.shortcuts import render, redirect
+from .models import Map
+from django.contrib.auth.decorators import login_required
+
+def tasks(request):
+    return render(request, "core/tasks.html")
+
+@login_required
+def user_maps(request):
+    maps = Map.objects.filter(creator=request.user)
+    for map in maps:
+        map.user_is_creator = (map.creator == request.user)
+    return render(request, 'core/user_maps.html', {'maps': maps})
+
+
+@login_required
+def public_maps(request):
+    maps = Map.objects.filter(is_public=True)
+    for map in maps:
+        map.user_likes = request.user in map.approval.all()
+    return render(request, 'core/public_maps.html', {'maps': maps})
+
+@login_required
+def toggle_public(request, map_id):
+    map = Map.objects.get(id=map_id)
+    if map.creator == request.user:
+        map.is_public = not map.is_public
+        map.save()
+    return redirect('user_maps')
+
+@login_required
+def like_map(request, map_id):
+    map = Map.objects.get(id=map_id)
+    if request.user not in map.approval.all():
+        map.approval.add(request.user)
+    else:
+        map.approval.remove(request.user)
+    return redirect('public_maps')
