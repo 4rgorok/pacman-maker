e766f8863b70fe1b9e1be1bc124b68c27b057988
poblos <zoltykropek@gmail.com>
2023-06-01 14:06:02 +0200

    Changed approval field to be manytomany

--- pacman/core/models.py
+++ pacman/core/models.py
@@ -6,5 +6,5 @@ class Map(models.Model):
     creator = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
     name = models.CharField(max_length = 100)
     creation_date = models.DateTimeField("creation date", default=timezone.now)
-    approval = models.IntegerField(default=0) # Idea is that this correlates to number of thumbs up under the map
+    approval = models.ManyToManyField(User) # Idea is that this correlates to number of thumbs up under the map
     layout = models.JSONField()
