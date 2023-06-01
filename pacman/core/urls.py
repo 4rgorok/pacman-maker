2ebe960a3e0d245b1068fb61c536af702913bcd7
poblos <zoltykropek@gmail.com>
2023-06-01 21:47:07 +0200

    Logout added

--- pacman/core/urls.py
+++ pacman/core/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 from .views import *
 from .views import SignUpView
+from django.contrib.auth.views import LogoutView
 
 app_name = 'core'
 urlpatterns = [
@@ -10,5 +11,6 @@ urlpatterns = [
     path("generator/", views.generator, name="generator"),
     path("game/", views.game, name="game"),
     path("login/", MyLoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(next_page='core:login'), name='logout'),
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
